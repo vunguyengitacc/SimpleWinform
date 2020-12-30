7155979eec580957b687760c081c622e5a9beaf3
nguyenhoanganhvu <nguyenhoanganhvu.it@gmail.com>
2020-12-30 20:28:20 +0700

    login event code

--- SimpleWinform/SimpleWinform/Form1.cs
+++ SimpleWinform/SimpleWinform/Form1.cs
@@ -19,8 +19,43 @@ namespace SimpleWinform
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            Infor ifPage = new Infor();
-            ifPage.ShowDialog();
+            if (txtUsername.Text == "username" && txtPassword.Text == "pass")
+            {
+                Infor ifPage = new Infor();
+                ifPage.ShowDialog();
+            }
+            else
+            {
+                DialogResult dr = MessageBox.Show("Tên tài khoản hoặc mật khẩu không chính xác!\nBạn có muốn nhập lại?", "Lỗi nhập", MessageBoxButtons.YesNo);
+                if(dr == DialogResult.No)
+                {
+                    Application.Exit();
+                }
+                else
+                {
+                    txtUsername.Text = "";
+                    txtPassword.Text = "";
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            checkLengTextBox(sender, txtUsername);
+        }
+
+        private void checkLengTextBox(object sender, TextBox txtBox)
+        {
+            if (txtBox.Text.Length > 20)
+            {
+                MessageBox.Show("Không được nhập quá 20 kí tự", "Lỗi nhập");
+                txtBox.Text = "";
+            }
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            checkLengTextBox(sender, txtPassword);
         }
     }
 }
